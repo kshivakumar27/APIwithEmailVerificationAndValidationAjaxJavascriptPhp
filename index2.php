2489508189644f9ca26f8a076c53394d05cbccae
Shiva Kumar K <shivakumar.k@ltts.com>
2022-06-03 15:14:36 +0530

    api

---
+++ index2.php
@@ -0,0 +1,72 @@
+<style>
+body{width:610px;}
+#frmContact {border-top:#F0F0F0 2px solid;background:#FAF8F8;padding:10px;}
+#frmContact div{margin-bottom: 15px}
+#frmContact div label{margin-left: 5px}
+.demoInputBox{padding:10px; border:#F0F0F0 1px solid; border-radius:4px;}
+.error{background-color: #FF6600;border:#AA4502 1px solid;padding: 5px 10px;color: #FFFFFF;border-radius:4px;}
+.success{background-color: #12CC1A;border:#0FA015 1px solid;padding: 5px 10px;color: #FFFFFF;border-radius:4px;}
+.info{font-size:.8em;color: #FF6600;letter-spacing:2px;padding-left:5px;}
+.btnAction{background-color:#2FC332;border:0;padding:10px 40px;color:#FFF;border:#F0F0F0 1px solid; border-radius:4px;}
+</style>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+<script>
+function sendContact() {
+	var valid;	
+	valid = validateContact();
+	if(valid) {
+		jQuery.ajax({
+		url: "https://api.quickemailverification.com/v1/verify?email=$userEmail&apikey=your api",
+		data:'userEmail='+$("#userEmail").val(),
+		type: "GET",
+		success:function(data){
+			
+print(data);
+//echo "email address is valid";
+
+ $decode=json_decode(data);
+
+ $reachable=($decode['success']=='true')? '<span class="badge badge-success">Valid</span>':'<span class="badge badge-danger">Invalid</span>' ;
+
+		$("#mail-status").html(data);
+		},
+		error:function (){}
+		});
+	}
+}
+
+function validateContact() {
+	var valid = true;	
+	$(".demoInputBox").css('background-color','');
+	$(".info").html('');
+	
+	
+	if(!$("#userEmail").val()) {
+		$("#userEmail-info").html("(required)");
+		$("#userEmail").css('background-color','#FFFFDF');
+		valid = false;
+	}
+	if(!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+		$("#userEmail-info").html("(invalid)");
+		$("#userEmail").css('background-color','#FFFFDF');
+		valid = false;
+	}
+
+	
+	return valid;
+}
+</script>
+<div id="frmContact">
+<div id="mail-status"></div>
+<div>
+
+<label>Email</label>
+<span id="userEmail-info" class="info"></span><br/>
+<input type="text" name="userEmail" id="userEmail" class="demoInputBox">
+</div>
+<div>
+
+<div>
+<button name="submit" class="btnAction" onClick="sendContact();">Send</button>
+</div>
+</div>
